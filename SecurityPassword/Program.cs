2d7d8653a971438d6aca554eff85c4cd8dde63e7
David <David@DAVE>
2022-10-30 20:43:28 +0200

    Add project files.

---
+++ SecurityPassword/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace SecurityPassword;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var generator = new OneTimePasswordGenerator();
+
+        Console.WriteLine("Please introduce your user id: ");
+        var userId = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(userId))
+        {
+            Console.WriteLine("The userId cannot be empty! Please introduce a value!");
+            return;
+        }
+
+        var generatedPassword = generator.GenerateOneTimePassword(userId, DateTime.Now);
+        Console.WriteLine($"The generated one time password is: {generatedPassword}!");
+
+        Console.WriteLine("Please validate your one time password: ");
+        var introducedPassword = Console.ReadLine();
+
+        var validationMessage = generator.ValidateSecurityPassword(userId, introducedPassword ?? string.Empty) ? "valid!" : "not valid!";
+        Console.WriteLine($"The introduced password is {validationMessage}!");
+    }
+}
+
+public class OneTimePasswordGenerator
+{
+    public SecurityService SecurityService;
+
+    public OneTimePasswordGenerator()
+    {
+        SecurityService = new SecurityService();
+    }
+
+    public string GenerateOneTimePassword(string userId, DateTime dateTime)
+    {
+        var securityPassword = SecurityService.GenerateOneTimePassword(userId, dateTime.ToString("MM/dd/yyyy hh:mm:ss"));
+
+        return securityPassword;
+    }
+
+    public bool ValidateSecurityPassword(string userId, string requestSecurityPassword)
+    {
+        var range = Enumerable.Range(0, 30);
+
+        foreach (var interval in range)
+        {
+            var candidatePassword = GenerateOneTimePassword(userId, DateTime.Now.AddSeconds(-1 * interval));
+            if (candidatePassword == requestSecurityPassword)
+                return true;
+        }
+
+        return false;
+    }
+}
+
---
+++ SecurityPassword/SecurityService.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SecurityPassword;
+
+public class SecurityService
+{
+    public string GenerateOneTimePassword(string userId, string dateTime)
+    {
+        return GenerateCode(userId, dateTime)[..6];
+    }
+
+    private static string GenerateCode(string userId, string dateTime)
+    {
+        return Math.Abs(BitConverter.ToInt32(Hash(userId, dateTime), 0)).ToString();
+    }
+
+    private static byte[] Hash(string userId, string dateTime)
+    {
+        return Hash(Encoding.Unicode.GetBytes(userId), Encoding.Unicode.GetBytes(dateTime));
+    }
+
+    private static byte[] Hash(IEnumerable<byte> userId, IEnumerable<byte> dateTime)
+    {
+        var saltedValue = userId.Concat(dateTime).ToArray();
+        var sha384 = SHA384.Create();
+
+        return sha384.ComputeHash(saltedValue);
+    }
+}
